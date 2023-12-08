df3808c5a916a94f8d540a8082497bb8ac61971a
Dima <goroshko5555@gmail.com>
2023-12-08 14:54:22 +0200

    change usloviya

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,45 +1,29 @@
 module.exports = {
-	'env': {
-		'browser': true,
-		'es2021': true
-	},
-	// 'extends': 'eslint:recommended',
-	'overrides': [
-		{
-			'env': {
-				'node': true
-			},
-			'files': [
-				'.eslintrc.{js,cjs}'
-			],
-			'parserOptions': {
-				'sourceType': 'script'
-			}
-		}
-	],
-	'parserOptions': {
-		'ecmaVersion': 'latest',
-		'sourceType': 'module'
-	},
-	'rules': {
-		'indent': [
-			'error',
-			'tab'
-		],
-		'linebreak-style': [
-			'error',
-			'windows'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'always'
-		],
-		'no-unused-vars' : [
-			'warn'
-		]
-	}
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  // 'extends': 'eslint:recommended',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    indent: ["error", "space"],
+    "linebreak-style": ["error", "windows"],
+    quotes: ["error", "single"],
+    semi: ["error", "always"],
+    "no-unused-vars": ["warn"],
+  },
 };
