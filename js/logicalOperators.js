1a77265ab6d9ee46e6617070ea9c296cb2e4e074
Dima <goroshko5555@gmail.com>
2023-12-08 15:01:36 +0200

    third commit

---
+++ js/logicalOperators.js
@@ -0,0 +1,8 @@
+const hamburger = 5;
+const fries = 0;
+
+if (hamburger && fries) {
+  console.log("I`m not hungry"); // логическое И
+}
+
+console.log(hamburger && fries);
---
+++ js/usloviya.js
@@ -0,0 +1,19 @@
+"use strict";
+
+if (4 == 9) {
+  console.log("OK!");
+} else {
+  console.log("Error!");
+}
+
+const num = 50;
+
+if (num < 49) {
+  console.log("error");
+} else if (num > 100) {
+  console.log("Eror");
+} else {
+  console.log("Ok");
+}
+
+num === 50 ? console.log("Super") : console.log("Fu"); // намбер равняется 50 ? тогда выводим в консоль супер, если нет(:) выводим Фу
